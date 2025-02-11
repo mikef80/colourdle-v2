d1426f1cbbed3b40b533d49e285a576644eb96ee
Mike <mike@mike-francis.org>
2025-02-11 21:59:11 +0000

    test stup done

--- prisma/seeds/run-seed.ts
+++ prisma/seeds/run-seed.ts
@@ -1,2 +1,10 @@
-const devData = import('../data/test-data/users');
-// const seed = import('./seed')
+import userData from "../data/test-data/users.ts";
+import gameData from "../data/test-data/games.ts";
+import resultData from "../data/test-data/results.ts";
+import seed from "./seed.ts";
+
+const runSeed = () => {
+  return seed({ userData, gameData, resultData });
+};
+
+runSeed();
--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -3,7 +3,32 @@ import userData from "../data/test-data/users.ts";
 import gameData from "../data/test-data/games.ts";
 import resultData from "../data/test-data/results.ts";
 
-async function main() {
+interface userData {
+  email: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+}
+
+interface gameData {
+  gameDate: string;
+  answer: number[];
+}
+
+interface resultData {
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  guesses: number[][];
+}
+
+interface seedData {
+  userData: userData[];
+  gameData: gameData[];
+  resultData: resultData[];
+}
+
+const seed = async ({ userData, gameData, resultData }: seedData) => {
   // Remove existing data
   await prisma.result.deleteMany();
   await prisma.game.deleteMany();
@@ -15,7 +40,9 @@ async function main() {
 
   // Seed Games
   await prisma.game.createMany({ data: gameData });
-  const gameIds = await prisma.game.findMany({ select: { id: true, answer: true, gameDate: true } });
+  const gameIds = await prisma.game.findMany({
+    select: { id: true, answer: true, gameDate: true },
+  });
 
   // Seed Results
   const results = await resultData.map((result, index) => {
@@ -24,29 +51,26 @@ async function main() {
       createdAt: result.createdAt,
       updatedAt: result.updatedAt,
       guesses: result.guesses,
-      userId: userIds[Math.floor(Math.random()*10)].id,
-      gameId: gameIds[Math.floor(Math.random()*10)].id,
+      userId: userIds[Math.floor(Math.random() * 10)].id,
+      gameId: gameIds[Math.floor(Math.random() * 10)].id,
       /* userId: userIds[index].id,
       gameId: gameIds[index].id, */
     };
-    // console.log(returnResult);
-    
-    return returnResult
-  })
-  
+
+    return returnResult;
+  });
 
   await prisma.result.createMany({
-    data: results
-  })
-  
-  console.log("Seeding complete!");
-}
+    data: results,
+  });
 
-main()
+  // console.log("Seeding complete!");
+};
+
+/* seed()
   .catch((e) => console.error(e))
   .finally(async () => {
     await prisma.$disconnect();
-  });
+  }); */
 
-
-// export default seed;
+export default seed;
