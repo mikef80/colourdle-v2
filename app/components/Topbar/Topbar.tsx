68881b25ebae46f41c5da4cbf081e3d23286fd1e
Mike <mike@mike-francis.org>
2025-08-20 21:53:04 +0100

    menu fade in/out is working correctly now, with padding instantly displayed on menu open, but delayed hide on close. also added so verificaiton code to check menu height on open and close

--- app/components/Topbar/Topbar.tsx
+++ app/components/Topbar/Topbar.tsx
@@ -1,6 +1,5 @@
-import { Link } from "react-router";
 import styles from "./Topbar.module.css";
-import { useState } from "react";
+
 import { useMenuStore } from "~/stores/useMenuStore";
 import { useAuth } from "~/utils/auth-context";
 import { supabase } from "~/utils/supabase.client";
@@ -15,6 +14,15 @@ const Topbar = () => {
   const loginVisible = useMenuStore((state) => state.loginVisible);
   const signupVisible = useMenuStore((state) => state.signupVisible);
   const menuOpen = useMenuStore((state) => state.menuOpen);
+
+  const toggleMenuVisiblity = () => {
+    toggleMenu();
+    if (menuOpen) {
+      console.log("true");
+    } else {
+      console.log("false");
+    }
+  };
 
   const logout = async () => {
     const { error } = await supabase.auth.signOut();
@@ -36,15 +44,12 @@ const Topbar = () => {
         <span className='letter-10 raleway-black'>!</span>
       </h1>
       <div className={styles.menuToggleContainer}>
-        <Hamburger direction='right' toggled={menuOpen} toggle={toggleMenu} />
+        <Hamburger direction='right' toggled={menuOpen} toggle={toggleMenuVisiblity} />
       </div>
       <nav className={`${styles.navbar} ${menuOpen ? styles.menuOpen : ""}`}>
         {!user && <button onClick={toggleSignup}>Signup</button>}
         {!user && <button onClick={toggleLogin}>Login</button>}
         {user && <button onClick={logout}>Logout</button>}
-
-        {/* <Link to={"/signup"}>Signup</Link>
-        <Link to={"/login"}>Login</Link> */}
       </nav>
       {!user && loginVisible && <Login />}
       {!user && signupVisible && <Signup />}
